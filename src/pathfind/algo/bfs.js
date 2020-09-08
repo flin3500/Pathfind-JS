62b8106fcf961d0cd2d9f372cc136e666f1d321c
Lin <lin.acandc@gmail.com>
2020-09-09 02:28:22 +1000

    add comment

--- src/pathfind/algo/bfs.js
+++ src/pathfind/algo/bfs.js
@@ -5,6 +5,7 @@
  * @param startNode
  * @param finishNode
  */
+func
 export default function Bfs(grid, startNode, finishNode){
     const bfsOrder = [];
     // 1. add start node to current node list
