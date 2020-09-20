97cfbf28ca75f3445a2f2d2bbb742baec32d41f7
Lin <lin.acandc@gmail.com>
2020-09-21 00:01:18 +1000

    add readme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 import Main from './pathfind/main'
 
+
+/**
+ * @author Lin
+ *
+ */
 export default function App() {
   return (
     <div className="App">
