59e045262948f5c116b69386301ae3f87470e301
Lin <lin.acandc@gmail.com>
2020-11-04 17:38:14 +1100

    add commit

--- src/pathfind/algo/asterTrue.js
+++ src/pathfind/algo/asterTrue.js
@@ -121,11 +121,7 @@ function findInList(findNode, list){
     return false;
 }
 
-/**
- * @author Lin
- *
- * @param node
- */
+
 function getPath(node){
     const finalPath = [];
     while(node){
